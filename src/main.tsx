171509acf456a11723e82f5928955c80f76b4395
petercascioli-droid <petercascioli@gmail.com>
2026-08-05 18:38:29 +0200

    Update and rename main.js to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.js';
+import App from './App';
+import './styles.css';
 
 const rootElement = document.getElementById('root');
 
 if (rootElement) {
   ReactDOM.createRoot(rootElement).render(
-    React.createElement(
-      React.StrictMode,
-      null,
-      React.createElement(App, null)
-    )
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   );
 }
