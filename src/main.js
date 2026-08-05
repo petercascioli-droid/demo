a698f032717a995e019947c6d14558fd49bcf68c
petercascioli-droid <petercascioli@gmail.com>
2026-08-05 18:30:32 +0200

    Update and rename main.jsx to main.js

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.js';
+import './styles.css';
+
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(
+    React.createElement(
+      React.StrictMode,
+      null,
+      React.createElement(App, null)
+    )
+  );
+}
